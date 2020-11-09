5554af4d59e0724c332864ee1f8e86eba60f066a
Manas Jain <manasj04@gmail.com>
2020-11-09 21:03:54 +0530

    Changed modal navigation button style

--- src/js/view/modal-view.js
+++ src/js/view/modal-view.js
@@ -2,9 +2,8 @@ import { elements, clearLoader } from "./base";
 
 export const renderImage = (imageUrl) => {
   elements.modalImageContainer.empty();
-  elements.modalImageContainer.append(
-    `<img class="theme-modal-image img-fluid js-modal-image" src="${imageUrl}">`
-  );
+  const html = `<img class="theme-modal-image img-fluid js-modal-image" src="${imageUrl}">`;
+  elements.modalImageContainer.append(html);
   elements.modalContainer.modal({ show: true });
   $(".js-modal-image").on("load", () => {
     clearLoader();
